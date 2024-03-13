69ab7b2d3f8c9b13bed78e073852322f49c287bb
TLu16 <tim.lucas6@hotmail.com>
2024-03-13 13:18:24 +0100

    Detailspage and services added

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,5 +8,22 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent {
-  heroes = HEROES;
+  heroes: Hero[] = []
+  selectedHero?: Hero
+
+  constructor(private heroService: HeroService) {
+
+  }
+
+  ngOnInit(): void {
+    this.getHeroes()
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(alf => this.heroes = alf)
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero
+  }
 }
